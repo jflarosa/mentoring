c9b2ddd3b7e9794fff7ed804ee8fedd5f1ba0668
Jean-François Larosa <jeanfrancois@adikteev.com>
2026-05-29 11:28:01 +0200

    feat: install react query axios and fetch weather api

--- src/features/creative-preview/components/PreviewCreativeStatic.tsx
+++ src/features/creative-preview/components/PreviewCreativeStatic.tsx
@@ -1,9 +1,36 @@
-import { Typography, Box } from '@ui';
+import { Box, Typography } from '@ui';
+import { useQuery } from '@tanstack/react-query';
+import {
+  getWeather,
+  getWeatherKey,
+} from '@features/creative-preview/api/getWeather.ts';
 
 export const PreviewCreativeStatic = () => {
+  const marseille = ['43.2965', '5.3698'] as const;
+  const paris = ['48.8566', '2.3522'] as const;
+
+  const marseilleQuery = useQuery({
+    queryKey: getWeatherKey(marseille),
+    queryFn: () => getWeather(marseille),
+  });
+
+  const parisQuery = useQuery({
+    queryKey: getWeatherKey(paris),
+    queryFn: () => getWeather(paris),
+  });
+
+  if (marseilleQuery.status === 'pending' || parisQuery.status === 'pending') {
+    return 'Fetch...';
+  }
+
+  if (marseilleQuery.status === 'error' || parisQuery.status === 'error') {
+    return <Typography color='error'>An error occurred</Typography>;
+  }
+
   return (
     <Box>
-      <Typography>Premier composant</Typography>
+      <Typography>Marseille :{marseilleQuery.data.latitude}</Typography>
+      <Typography>Paris :{parisQuery.data.latitude}</Typography>
     </Box>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,17 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CssBaseline />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
